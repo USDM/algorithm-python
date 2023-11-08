5aa395683b18aa273996904945252bde3ee6ec38
Uriel Sanchez <uri-19982609@hotmail.com>
2023-11-07 20:48:43 -0600

    Se comenzo circle

---
+++ bresenhamCircle/bresenhamCircle.py
@@ -0,0 +1,25 @@
+class Point:
+  def __init__(self, x,y):
+    self.x=x
+    self.y=y
+
+class BresenhamCircle:
+
+  def __init__(self, center, radius):
+    self.center = center
+    self.radius = radius
+    pass
+
+  def draw_circle(self):
+    x=0
+    y=radius
+    while x != y:
+
+  def _plot_point(self, point):
+
+
+
+center = 
+radius = 5
+bresenhamCircle = BresenhamCircle()
+bresenhamCircle.draw_circle(center, radius)
